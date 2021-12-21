c0d97ca2685f53c8ad6dbd19b94b8629b8fcd8cc
nileshkumar0726 <kumar.nilesh0726@gmail.com>
2021-12-21 12:23:09 -0500

    Added custom class for dataset for loading src and target pairs

--- Constants.py
+++ Constants.py
@@ -1,2 +1,9 @@
 device_T = 'gpu'
 tess_size = [4,4]
+IMG_FOLDER = "/home/nk4856/data/Datasets/Cardiac_Dataset/LV_Dataset/LV_Labels"
+
+epochs = 400
+batch_size = 16
+lr = 0.001 
+img_dimensions = (200,200)
+regularization_constant = 1
